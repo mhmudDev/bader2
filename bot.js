f8022cfe61f81a030244679ef092fc858d027999
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-06-04 11:25:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("583623939503292426");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**__W__elcome __T__o __D__ark__S__ide :Up_Logo: __P__rove __Y__our __S__elf __I__n #lobby :_Heart: **`), 4000)        
+setTimeout(() => mohamed.send(`**Welcome To Darkside :leaves:  **`), 4000)        
 }
 });
 
